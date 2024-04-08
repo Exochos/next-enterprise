018e4bb13031d566807cc9201195dcc929928ec6
Jeremy Ward <exochos@gmail.com>
2024-04-08 12:12:41 -0700

    asdsad

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ import { env } from "./env.mjs"
  * @type {import('next').NextConfig}
  */
 const config = withPlugins([[withBundleAnalyzer({ enabled: env.ANALYZE })]], {
+  output: "export",
   reactStrictMode: true,
   experimental: { instrumentationHook: true },
   rewrites() {
